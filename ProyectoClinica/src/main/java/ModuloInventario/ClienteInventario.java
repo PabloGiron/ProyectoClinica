b2801c181af50637ac18ce9330796b782587eda9
PabloG <pablo.giron.97@gmail.com>
2019-07-09 01:07:02 -0600

    Creacion del cliente
    :tada:

--- ProyectoClinica/src/main/java/ModuloInventario/ClienteInventario.java
+++ ProyectoClinica/src/main/java/ModuloInventario/ClienteInventario.java
@@ -15,12 +15,12 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author oem
  */
-public class ClienteVentas extends javax.swing.JFrame {
+public class ClienteInventario extends javax.swing.JFrame {
 
     /**
      * Creates new form ClienteVentas
      */
-    public ClienteVentas() {
+    public ClienteInventario() {
         initComponents();
         llenarTabla();
         cargarDatos();
@@ -405,20 +405,21 @@ public class ClienteVentas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClienteVentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClienteInventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClienteVentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClienteInventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClienteVentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClienteInventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClienteVentas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ClienteInventario.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ClienteVentas().setVisible(true);
+                new ClienteInventario().setVisible(true);
             }
         });
     }
