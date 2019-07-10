7d1005181e26777d67ba85b1a5fc1418c7f6426f
Andrés <nsanchezandresca@gmail.com>
2019-07-10 03:16:44 -0600

    Cambios a clases y termino de servicios

---
+++ ProyectoClinica/src/main/java/ModelosTablas/ModeloTablaServicios.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModelosTablas;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author nasc_
+ */
+public class ModeloTablaServicios extends ACModeloTabla{
+
+    @Override
+    public DefaultTableModel getModelo() {
+    DefaultTableModel modelo = null;
+        try {
+            modelo = (new DefaultTableModel(
+                    null, new String[]{
+                        "", "Nombre",
+                        "Precio"}) {
+                Class[] types = new Class[]{
+                    java.lang.String.class,
+                    java.lang.String.class,
+                    java.lang.String.class
+                };
+                boolean[] canEdit = new boolean[]{
+                    false, false, false
+                };
+                
+                @Override
+                public Class getColumnClass(int columnIndex) {
+                    return types[columnIndex];
+                }
+
+                @Override
+                public boolean isCellEditable(int rowIndex, int colIndex) {
+                    return canEdit[colIndex];
+                }
+            });
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return modelo;
+    }
+    
+}
--- ProyectoClinica/src/main/java/ModelosTablas/ModeloTablaVentas.java
+++ ProyectoClinica/src/main/java/ModelosTablas/ModeloTablaVentas.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ModuloVentas;
+package ModelosTablas;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
