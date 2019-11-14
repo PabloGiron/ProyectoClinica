4d5e06671d695e1870689b12a46ba6341ee873d0
JP <pablo.giron.97@gmail.com>
2019-11-13 18:33:46 -0600

    Eliminación de archivos de la rama master
    :bear:

--- ProyectoClinica/src/main/java/Main/Main.java
+++
@@ -1,9 +0,0 @@
-package Main;
-
-//METODO INICIAL SOLO LLAMA AL FRAME INICIAL
-public class Main {
-    public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        cliente.setVisible(true);
-    }
-}
